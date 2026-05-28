f2094f4681b448c7ff82f65240d3b02b764dd5cd
unknown <leijanuelalfarocayanan@gmail.com>
2026-05-28 16:14:49 +0800

    lei

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -6,11 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class employee extends Model
 {
-    use HasFactory;
-
-    protected $table = "employeetbl";
-    protected $primarykey= "id";
-    protected $fillable =[
-        'fname','mname','lname','address','dob','contactno'
-    ];
+    protected $table = 'employee';
+    protected $fillable = ['name', 'email', 'phone', 'address', 'position'];        
+    
 }
--- routes/web.php
+++ routes/web.php
@@ -18,12 +18,12 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 Route::middleware('auth')->group(function () {
     Route::view('about', 'about')->name('about');
 
-    Route::get('employee', [App\Http\Controllers\employeecontroller::class, 'index'])->name(employee.index);
-    Route::get('employee/create', [App\Http\Controllers\employeecontroller::class, 'create'])->name(employee.create);
-    Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'store'])->name(employee.store);
-    Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit'])->name(employee.edit);
-    Route::pull('employee/update', [App\Http\Controllers\employeecontroller::class, 'update'])->name(employee.update);
-Route::delete('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete'])->name(employee.delete);
+    Route::get('employee', [App\Http\Controllers\employeecontroller::class, 'index'])->name('employee.index');
+    Route::get('employee/create', [App\Http\Controllers\employeecontroller::class, 'create'])->name('employee.create');
+    Route::post('employee', [App\Http\Controllers\employeecontroller::class, 'store'])->name('employee.store');
+    Route::get('employee/{id}/edit', [App\Http\Controllers\employeecontroller::class, 'edit'])->name('employee.edit');
+    Route::put('employee/{id}/update', [App\Http\Controllers\employeecontroller::class, 'update'])->name('employee.update');
+    Route::delete('employee/{id}/delete', [App\Http\Controllers\employeecontroller::class, 'delete'])->name('employee.delete');
 
     Route::get('users', [\App\Http\Controllers\UserController::class, 'index'])->name('users.index');
 
