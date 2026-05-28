4482c3f223ca96074773451114eb7c5e4d48b5f2
unknown <leijanuelalfarocayanan@gmail.com>
2026-05-28 16:34:29 +0800

    pull rqst

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 class employee extends Model
 {
     protected $table = 'employee';
-    protected $fillable = ['name', 'email', 'phone', 'address', 'position'];        
+    protected $fillable = ['name', 'email', 'phone', 'address', 'position'];
+    protected $primaryKey = 'id';
+    public $timestamps = false;
+    public function getRouteKeyName()
+    {
+        return 'id';
+    }
+
+    
     
 }
